5d2b1ff1a4e21bff9585a89cfe90706f3a7c21b7
Simon <info@simon-vedder.de>
2025-09-13 14:05:01 +0200

    init push

---
+++ terraform/environments/dev/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg"
+    storage_account_name = "thisisatestsaacc"  # Dein Storage Account Name
+    container_name       = "tfstatefiles"
+    key                  = "dev/terraform.tfstate"
+  }
+}
---
+++ terraform/environments/dev/main.tf
@@ -0,0 +1,123 @@
+# terraform/environments/dev/main.tf
+terraform {
+  required_version = ">= 1.6.0"
+  
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.80"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
+
+# Lokale Variablen
+locals {
+  environment = "dev"
+  location    = "West Europe"
+  
+  tags = {
+    Environment = local.environment
+    Project     = "terraform-template"
+    ManagedBy   = "Terraform"
+  }
+  
+  resource_prefix = "demo-${local.environment}"
+}
+
+# Resource Group
+resource "azurerm_resource_group" "main" {
+  name     = "${local.resource_prefix}-rg"
+  location = local.location
+  tags     = local.tags
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "main" {
+  name                = "vnet-${local.resource_prefix}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tags                = local.tags
+}
+
+# Subnet
+resource "azurerm_subnet" "internal" {
+  name                 = "subnet-internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Network Security Group
+resource "azurerm_network_security_group" "main" {
+  name                = "nsg-${local.resource_prefix}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tags                = local.tags
+
+  security_rule {
+    name                       = "AllowRDP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+
+# Network Interface
+resource "azurerm_network_interface" "main" {
+  name                = "nic-${local.resource_prefix}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tags                = local.tags
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Subnet NSG Association
+resource "azurerm_subnet_network_security_group_association" "main" {
+  subnet_id                 = azurerm_subnet.internal.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
+
+# Virtual Machine
+resource "azurerm_windows_virtual_machine" "main" {
+  name                = "vm-${local.resource_prefix}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  admin_password      = "Test12345678" 
+  tags                = local.tags
+
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2025-datacenter-g2"
+    version   = "latest"
+  }
+}
