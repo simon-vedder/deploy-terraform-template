88b6805faac7f646e5e148548e11f855e171f14e
Simon <info@simon-vedder.de>
2025-09-14 12:57:36 +0200

    update

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,8 +1,9 @@
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "rg"
-#     storage_account_name = "thisisatestsaacc"  # Dein Storage Account Name
-#     container_name       = "tfstatefiles"
-#     key                  = "dev/terraform.tfstate"
-#   }
-# }
+terraform {
+  backend "azurerm" {
+    use_oidc             = true
+    resource_group_name  = "rg"
+    storage_account_name = "thisisatestsaacc"  # Dein Storage Account Name
+    container_name       = "tfstatefiles"
+    key                  = "dev/terraform.tfstate" 
+  }
+}
