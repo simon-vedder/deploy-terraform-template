4788b39e67b59e511056412b17a7e591273a2d2b
Simon <info@simon-vedder.de>
2025-10-04 20:31:58 +0200

    backup update

--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -4,6 +4,6 @@ terraform {
     resource_group_name  = "rg"
     storage_account_name = "thisisatestsaacc" # Your storage account name for state files
     container_name       = "tfstatefiles"
-    key                  = "prod/terraform.tfstate" 
+    key                  = "prod/terraform.tfstate"
   }
 }
